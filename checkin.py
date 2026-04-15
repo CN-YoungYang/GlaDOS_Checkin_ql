f79c0f56c5ffc4c5e6bb9a2f3d7541907aacaa82
CN-YoungYang <25340441+CN-YoungYang@users.noreply.github.com>
2026-04-15 11:25:29 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -61,7 +61,7 @@ def checkin(cookie):
     origin = "https://glados.rocks"
     useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
     payload={
-        'token': 'glados.one'
+        'token': 'glados.rocks'
     }
     try:
         checkin = requests.post(checkin_url,headers={
